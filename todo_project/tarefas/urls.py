f4f662dc470c63be4ef192fe40f2ec2c2991c3c4
Juan Blanco <jfblancoregueira@gmail.com>
2026-03-19 13:20:14 -0300

    Adição de um sistema de edição de tarefas + Melhorias na UI

--- todo_project/tarefas/urls.py
+++ todo_project/tarefas/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.lista_tarefas, name='lista'),
     path('criar/', views.criar_tarefa, name='criar'),
+    path('editar/<int:id>/', views.editar_tarefa, name='editar'),
     path('excluir/<int:id>/', views.excluir_tarefa, name='excluir'),
 ]
--- todo_project/tarefas/views.py
+++ todo_project/tarefas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 from .models import Tarefa
 from .forms import TarefaForm
 
@@ -19,6 +20,18 @@ def criar_tarefa(request):
 
     return render(request, 'tarefas/criar.html', {'form': form})
 
+def editar_tarefa(request, id):
+    tarefa = get_object_or_404(Tarefa, id=id)
+
+    if request.method == 'POST':
+        form = TarefaForm(request.POST, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            return redirect('lista')
+    else:
+        form = TarefaForm(instance=tarefa)
+
+    return render(request, 'tarefas/editar.html', {'form': form})
 
 def excluir_tarefa(request, id):
     tarefa = Tarefa.objects.get(id=id)
